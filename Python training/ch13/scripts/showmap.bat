dce17311dfa379e20ad49d322b2336d624354e05
NazmulHoque1010 <mohammad.hoque1000@gmail.com>
2026-09-20 12:57:54 +0600

    Project 6 from ch13 done

---
+++ Python training/ch13/scripts/showmap.bat
@@ -0,0 +1,3 @@
+@call "C:\Users\Lenovo\Desktop\IC\python training\ch13\scripts\.venv\Scripts\activate.bat"
+@python "C:\Users\Lenovo\Desktop\IC\python training\ch13\scripts\showmap.py" %*
+@deactivate
